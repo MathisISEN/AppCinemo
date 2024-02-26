6ab7b3474e814d90fea797a36ff224bdedd30639
MathisISEN <mathis.vandevenne@student.junia.com>
2024-02-26 02:34:42 +0100

    git commit appcinemo(activity/Manifest): add feature => MainActivity / MovieDetailsActivity / SeeAllMovieActivity / SplashScreenActivity / YoutubeVideoPlayerActivity / AndroidManifest

---
+++ app/src/main/java/com/example/appcinemo/AppCinemo.kt
@@ -0,0 +1,8 @@
+package com.example.appcinemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppCinemo : Application() {
+}
--- app/src/main/java/com/example/appcinemo/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/appcinemo/ui/activity/SplashScreenActivity.kt
@@ -1,4 +1,22 @@
 package com.example.appcinemo.ui.activity
 
-class SplashScreenActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.appcinemo.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 1000)
+    }
 }
