7702c996461a71c82b68fe831cfc68e3fa6a70ed
MathisISEN <mathis.vandevenne@student.junia.com>
2024-02-26 01:24:33 +0100

    git commit res(menu/layout): add => all layout / build gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,14 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.material:material:1.5.0")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:10.0.5")
+    implementation ("com.github.HamidrezaAmz:MagicalExoPlayer:3.0.5")
+    implementation ("com.airbnb.android:lottie:4.2.1")
+
+
+
 }
